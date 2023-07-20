ad46a8c960ceb8a733077c199f1f9c3127619de2
cognivore <66186054+cognivore@users.noreply.github.com>
2023-07-20 13:04:23 +0100

    1.14

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Ubuntu.MixProject do
     [
       app: :ubuntu,
       version: "20.4.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
